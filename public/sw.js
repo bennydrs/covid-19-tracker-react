3179b525cc88eb16aff664e287ca5c348f5bfbdf
benny <bennydrm16@gmail.com>
2021-07-28 09:21:52 +0700

    fix sw

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,12 @@
 const CACHE_NAME = "version-1"
 const urlsToCache = ["index.html", "offline.html"]
 
+// @ts-ignore
 const self = this
 
 // Install SW
 self.addEventListener("install", (event) => {
+  // @ts-ignore
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       console.log("Opened cache")
@@ -16,8 +18,11 @@ self.addEventListener("install", (event) => {
 
 // Listen for requests
 self.addEventListener("fetch", (event) => {
+  // @ts-ignore
   event.respondWith(
+    // @ts-ignore
     caches.match(event.request).then(() => {
+      // @ts-ignore
       return fetch(event.request).catch(() => caches.match("offline.html"))
     })
   )
@@ -28,9 +33,11 @@ self.addEventListener("activate", (event) => {
   const cacheWhitelist = []
   cacheWhitelist.push(CACHE_NAME)
 
+  // @ts-ignore
   event.waitUntil(
     caches.keys().then((cacheNames) =>
       Promise.all(
+        // eslint-disable-next-line array-callback-return
         cacheNames.map((cacheName) => {
           if (!cacheWhitelist.includes(cacheName)) {
             return caches.delete(cacheName)
